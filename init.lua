fad71defe1629e128a876f16ff4027f8d60fb21e
shamil9 <shamil9@gmail.com>
2022-07-17 20:17:56 +0200

    setup init

---
+++ init.lua
@@ -0,0 +1,13 @@
+local config = {
+  colorscheme = "nordfox",
+  enabled = require "user.core.default_plugins",
+  ui = require "user.core.ui",
+  plugins = require "user.plugins.plugins",
+  luasnip = require "user.plugins.luasnip",
+  ["which-key"] = require "user.plugins.which-key",
+  lsp = require "user.plugins.lsp",
+  diagnostics = require "user.core.diagnostics",
+  ["null-ls"] = require "user.plugins.null-ls",
+  polish = require "user.core.polish",
+}
+return config
